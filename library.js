fa6e08c05b86047e2bea67e4b45e897cc4d33a40
rudy <rudolf.zachar@gmail.com>
2020-09-13 13:10:06 +0100

    hw_ES6_features class Library and Book

---
+++ library.js
@@ -0,0 +1,48 @@
+class Library {
+  constructor (books = []) {
+  this.books = books;
+  };
+
+// Library.prototype.bookCount = function () {
+//   return this.books.length;
+// };
+
+bookCount () {
+  return this.books.length;
+};
+
+// Library.prototype.addBook = function (newBook) {
+//   this.books.push(newBook);
+// };
+
+addBook(newBook) {
+  this.books.push(newBook);
+};
+
+// Library.prototype.addBooks = function (newBooks) {
+//   newBooks.forEach(book => this.books.push(book));
+// };
+
+addBooks(newBooks) {
+  newBooks.forEach(book => this.books.push(book));
+};
+
+// Library.prototype.printInventory = function () {
+//   this.books.forEach((book) => {
+//     console.log(`${book.title} by ${book.author}`);
+//   });
+// };
+
+printInventory() {
+  this.books.forEach((book) => {
+    var title = book.title;
+    var author = book.aurthor;
+    console.log(`${title} by ${author}`);
+  });
+}
+}
+
+
+export default Library;
+
+
